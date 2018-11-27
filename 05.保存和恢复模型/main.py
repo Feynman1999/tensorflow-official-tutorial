fa349457d760a4b2b31448f2e47e3c837a1bd42b
Yuxiang Chen <chenyx.cs@gmail.com>
2018-11-27 18:12:51 +0800

    。。

--- 05.保存和恢复模型/main.py
+++ 05.保存和恢复模型/main.py
@@ -19,7 +19,8 @@ def create_model():
         keras.layers.Dropout(0.2),
         keras.layers.Dense(10,activation=tf.nn.softmax)
     ])
-    model.compile(optimizer=tf.train.AdamOptimizer(),
+    model.compile(optimizer=keras.optimizers.Adam(), # tf.train.AdamOptimizer(), 如果使用tf的优化器 不能随模型整个保存 
+    #但好像tf在load_weights时会保存优化器 然而keras的不会  (⊙o⊙)… 之后可能会合并
                   loss='sparse_categorical_crossentropy',
                   metrics=['accuracy'])
     return model
@@ -88,5 +89,47 @@ checkpoints = pathlib.Path(checkpoint_dir).glob("*.index")
 checkpoints = sorted(checkpoints, key=lambda cp:cp.stat().st_mtime)
 checkpoints = [cp.with_suffix('') for cp in checkpoints]
 latest = str(checkpoints[-1])
-print(checkpoints,'\n',latest)
+# print(checkpoints,'\n',latest)
 # 默认的 TensorFlow 格式仅保存最近的 5 个检查点。
+print(latest)
+model = create_model()
+model.load_weights(latest) # 最新的参数
+loss, acc = model.evaluate(test_images, test_labels)
+print("Restored model, accuracy: {:5.2f}%".format(100*acc))
+
+
+# 直接用save_weights方法也可以
+# Save the weights
+model.save_weights('./checkpoints/my_checkpoint')
+# Restore the weights
+model = create_model()
+model.load_weights('./checkpoints/my_checkpoint')
+
+loss,acc = model.evaluate(test_images, test_labels)
+print("Restored model, accuracy: {:5.2f}%".format(100*acc))
+
+
+
+
+
+'''
+保存整个模型
+'''
+# 整个模型可以保存到一个文件中，其中包含权重值、模型配置乃至优化器配置
+# 这样，就可以为模型设置检查点，并稍后从完全相同的状态继续训练，而无需访问原始代码。
+# 在 Keras 中保存完全可正常使用的模型非常有用，
+# 可以在 TensorFlow.js 中加载它们，然后在网络浏览器中训练和运行它们。
+
+# Keras 使用 HDF5 标准提供基本的保存格式。对于我们来说，可将保存的模型视为一个二进制 blob。
+model = create_model()
+model.fit(train_images, train_labels, epochs=5)
+model.save('my_model.h5')
+
+# Recreate the exact same model, including weights and optimizer.
+new_model = keras.models.load_model('my_model.h5')
+new_model.summary()
+loss, acc = new_model.evaluate(test_images, test_labels)
+print("Restored model, accuracy: {:5.2f}%".format(100*acc))
+# 权重值  模型配置（架构） 优化器配置 均会保存
+# Keras 通过检查架构来保存模型。目前，它无法保存 TensorFlow 优化器（来自 tf.train）。
+# 更多内容参看https://www.tensorflow.org/guide/keras
